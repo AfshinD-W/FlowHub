28caf59ea0e8e6325b078f85e2cf31760cb77f94
jafari <afshinaa705@gmail.com>
2026-07-13 15:59:07 +0330

    refactor(identity): change the title of swagger ui doc

--- src/Host/FlowHub.Host/Program.cs
+++ src/Host/FlowHub.Host/Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "AuthLab v1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FlowHub v1");
         c.RoutePrefix = string.Empty;
     });
 }
