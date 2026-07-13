82e1a11c1019296563ca97194b8379e3f3c0e51c
jafari <afshinaa705@gmail.com>
2026-07-13 14:09:19 +0330

    refactor(identity): change the name AppDbContext to IdentityDbContext

---
+++ src/Modules/Identity/FlowHub.Modules.Identity.Infrastructure/Database/IdentityDbContext.cs
@@ -0,0 +1,11 @@
+﻿using FlowHub.Modules.Identity.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowHub.Modules.Identity.Infrastructure.Database
+{
+    public class IdentityDbContext : IdentityDbContext<User, Role, string>
+    {
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options) { }
+    }
+}
