8c8bfeca5c44a4815112a9f23b52a414eecf1f12
jafari <afshinaa705@gmail.com>
2026-07-13 15:16:34 +0330

    feat(identity): add swagger & DependencyInjection for infrastructure of identity

---
+++ src/Modules/Identity/FlowHub.Modules.Identity.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using FlowHub.Modules.Identity.Infrastructure.Database;
+using FlowHub.Modules.Identity.Infrastructure.Identity.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlowHub.Modules.Identity.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddIdentityInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            //DataBase
+            services.AddDbContext<IdentityDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("SqlServer")));
+
+            services.AddIdentity<User, Role>().AddEntityFrameworkStores<IdentityDbContext>();
+
+            return services;
+        }
+    }
+}
